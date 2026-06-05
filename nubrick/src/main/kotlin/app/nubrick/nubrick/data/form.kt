9e5abc18704a9ccd0788e6545c87c1d772380330
Victor Bayona <victor.bayona@plaid.co.jp>
2026-06-05 18:00:43 +0900

    Remove manual form value listeners in favor of StateFlow collection

--- nubrick/src/main/kotlin/app/nubrick/nubrick/data/form.kt
+++ nubrick/src/main/kotlin/app/nubrick/nubrick/data/form.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import java.util.concurrent.CopyOnWriteArraySet
 
 internal sealed class FormValue {
     class Bool(val bool: Boolean) : FormValue()
@@ -23,22 +22,16 @@ internal sealed class FormValue {
     }
 }
 
-typealias FormValueListener = (values: Map<String, JsonElement>) -> Unit
-
-
 internal interface FormRepository {
     val formValues: StateFlow<Map<String, FormValue>>
     fun getFormData(): Map<String, JsonElement>
     fun setValue(key: String, value: FormValue)
     fun getValue(key: String): FormValue?
-    fun addListener(listener: FormValueListener)
-    fun removeListener(listener: FormValueListener)
 }
 
 internal class FormRepositoryImpl : FormRepository {
     private val _formValues = MutableStateFlow<Map<String, FormValue>>(emptyMap())
     override val formValues: StateFlow<Map<String, FormValue>> = _formValues.asStateFlow()
-    private val listeners: MutableSet<FormValueListener> = CopyOnWriteArraySet()
 
     override fun getFormData(): Map<String, JsonElement> {
         return this.formValues.value.toFormData()
@@ -50,20 +43,6 @@ internal class FormRepositoryImpl : FormRepository {
 
     override fun setValue(key: String, value: FormValue) {
         this._formValues.update { it + (key to value) }
-        val formData = this._formValues.value.toFormData()
-        
-        // Listeners to be updated to simply collect StateFlow in future PR
-        listeners.forEach { listener ->
-            listener(formData)
-        }
-    }
-
-    override fun addListener(listener: FormValueListener) {
-        this.listeners.add(listener)
-    }
-
-    override fun removeListener(listener: FormValueListener) {
-        this.listeners.remove(listener)
     }
 }
 
