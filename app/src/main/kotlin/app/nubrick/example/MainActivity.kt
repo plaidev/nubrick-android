9d37035b635225d741b0eed0a00a53ae091318a3
Victor Bayona <62053428+VictorB00@users.noreply.github.com>
2026-08-21 16:01:32 +0900

    Remove embedding size and enable edge to edge insets (#160)

--- app/src/main/kotlin/app/nubrick/example/MainActivity.kt
+++ app/src/main/kotlin/app/nubrick/example/MainActivity.kt
@@ -3,10 +3,11 @@ package app.nubrick.example
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
@@ -16,13 +17,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import app.nubrick.example.ui.theme.NubrickAndroidTheme
 import app.nubrick.nubrick.NubrickProvider
 import app.nubrick.nubrick.NubrickSDK
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             NubrickAndroidTheme {
@@ -33,19 +34,19 @@ class MainActivity : ComponentActivity() {
                         color = MaterialTheme.colorScheme.background
                     ) {
                         Column(
-                            modifier = Modifier.verticalScroll(rememberScrollState()),
+                            modifier = Modifier
+                                .systemBarsPadding()
+                                .verticalScroll(rememberScrollState()),
                             horizontalAlignment = Alignment.CenterHorizontally,
                             verticalArrangement = Arrangement.Top,
                         ) {
                             NubrickSDK.Embedding(
                                 "HEADER_INFORMATION",
                                 arguments = emptyMap<String, String>(),
-                                modifier = Modifier.height(100f.dp),
                             )
                             NubrickSDK.Embedding(
                                 "TOP_COMPONENT",
                                 arguments = emptyMap<String, String>(),
-                                modifier = Modifier.height(270f.dp),
                             )
                         }
                     }
